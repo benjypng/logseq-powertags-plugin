2657b3555cc6448786a14dceb71a4a0eb2db5615
Benjy <ben@pngs.cc>
2024-08-04 19:54:41 +0800

    initla commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,79 +1,41 @@
-import "@logseq/libs";
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import CreateTag from "./components/CreateTag";
-import settings from "./services/callSettings";
-import observerCallback from "./services/observerCallback";
-import findTag from "./utils/findTag";
-import handleListeners from "./utils/handleListeners";
-import * as te from "tw-elements";
+import '@logseq/libs'
 
-function main() {
-  console.log("logseq-powertags-plugin loaded");
+import { createRoot } from 'react-dom/client'
 
-  handleListeners();
+import PowerTags from './components/PowerTags'
+import { handlePopup } from './handle-popup'
+import settings from './services/callSettings'
+import observerCallback from './services/observerCallback'
 
-  // Create setup
-  if (!logseq.settings!.savedTags) {
-    logseq.updateSettings({
-      savedTags: {},
-    });
-  }
-  if (!logseq.settings!.savedBlk) {
-    logseq.updateSettings({
-      savedBlks: {},
-    });
-  }
+const main = () => {
+  console.log('logseq-powertags-plugin loaded')
+  handlePopup()
 
-  // CREATE TAG
-  logseq.Editor.registerSlashCommand("Create power tag", async function () {
-    const content = await logseq.Editor.getEditingBlockContent();
-    const tag = findTag(content);
-
-    ReactDOM.render(
-      <React.StrictMode>
-        <CreateTag tag={tag} />;
-      </React.StrictMode>,
-      document.getElementById("app")
-    );
-    logseq.showMainUI();
-  });
+  const el = document.getElementById('app')
+  if (!el) return
+  const root = createRoot(el)
+  root.render(<PowerTags />)
 
   // MANAGE TAGS
   logseq.provideModel({
-    async manageTag() {
-      const mappedSavedTags = Object.entries(logseq.settings!.savedTags).map(
-        (i) => ({
-          tag: i[0],
-          properties: i[1],
-        })
-      );
-
-      ReactDOM.render(
-        <React.StrictMode>
-          <App savedTags={mappedSavedTags} />
-        </React.StrictMode>,
-        document.getElementById("app")
-      );
-      logseq.showMainUI();
+    async managePowerTags() {
+      logseq.showMainUI()
     },
-  });
+  })
 
-  logseq.App.registerUIItem("toolbar", {
-    key: "logseq-powertags-plugin",
-    template: `<a data-on-click="manageTag" class="button"><i class="ti ti-tag"></i></a>`,
-  });
+  logseq.App.registerUIItem('toolbar', {
+    key: 'logseq-powertags-plugin',
+    template: `<a data-on-click="managePowerTags" class="button"><i class="ti ti-tag"></i></a>`,
+  })
 
-  //@ts-expect-error
-  const observer = new top.MutationObserver(observerCallback);
+  //@ts-expect-error need to access Logseq parent
+  const observer = new parent.MutationObserver(observerCallback)
 
-  //@ts-expect-error
-  observer.observe(top.document.getElementById("app-container"), {
+  observer.observe(parent.document.getElementById('app-container'), {
     attributes: false,
     childList: true,
     subtree: true,
-  });
+  })
 }
 
-logseq.useSettingsSchema(settings).ready(main).catch(console.error);
+logseq.useSettingsSchema(settings).ready(main).catch(console.error)
