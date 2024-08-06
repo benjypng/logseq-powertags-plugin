24f2aa99fbba8730bb90d2fca042f025e71ee0b6
Benjy <ben@pngs.cc>
2024-08-06 11:17:20 +0800

    fix: only change parent blocks

--- src/services/core/query-blocks-with-tag.ts
+++ src/services/core/query-blocks-with-tag.ts
@@ -3,11 +3,15 @@ import { BlockEntity } from '@logseq/libs/dist/LSPlugin'
 export const queryBlocksWithTag = async (
   tag: string,
 ): Promise<BlockEntity[] | null> => {
-  const blocks = await logseq.DB.q(`[[${tag}]]`)
+  let blocks = await logseq.DB.q(`[[${tag}]]`)
   if (!blocks || blocks.length === 0) {
     await logseq.UI.showMsg(`No blocks with PowerTag: ${tag} found`)
     logseq.hideMainUI()
     return null
   }
+  // Only retrieve blocks where its parent is the page so that child blocks are not selected and properties are being added to child blocks
+  blocks = blocks.filter(
+    (block: BlockEntity) => block.page.id === block.parent.id,
+  )
   return blocks
 }
